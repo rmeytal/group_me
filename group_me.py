d89fea5a7cf16a89d956e6f1b968ea64c6aef1e8
Rei Meytal <reimeytal52@gmail.com>
2026-02-15 11:33:20 +0200

    fixed

--- group_me.py
+++ group_me.py
@@ -9,11 +9,15 @@ def group(f: Callable, vals: List) -> Dict:
     ret = {}
 
     for val in vals:
-        ret[val] = f(val)
+        func_result = f(val)
+        if func_result in ret.keys():
+            ret[func_result].append(val)
+        else:
+            ret[func_result] = [val]
     return ret
 
 def mod_three(num: int) -> int:
     return num % 3
 
 if __name__ == "__main__":
-    print(group(mod_three, [0, 1, 2, 4, 5, 6]))
+    print(group(mod_three, [1, 3, 4, 5, 7, 9]))
